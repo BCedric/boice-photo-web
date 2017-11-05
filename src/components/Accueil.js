69e8a6cca6855ae8238a5cc27b67f094856f4cab
BCedric <you@example.com>
2017-11-05 02:18:46 +0100

    plop

--- src/components/Accueil.js
+++ src/components/Accueil.js
@@ -4,6 +4,9 @@ export default function Accueil () {
   return (
     <div>
       <h1>Accueil</h1>
+      <p className="App-intro">
+      To get started, edit <code>src/App.js</code> and save to reload.
+    </p>
     </div>
   )
 }
