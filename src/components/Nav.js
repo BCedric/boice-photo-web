df186ff4a401dc035d4f9462b0f6d26254deba65
BCedric <you@example.com>
2017-11-21 01:12:46 +0100

    add pages for galleries displayed in nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,21 +3,23 @@ import {
   Link
 } from 'react-router-dom'
 import PropTypes from 'prop-types'
-import { map, upperFirst } from 'lodash'
+import { map, upperFirst, remove, findIndex } from 'lodash'
 import { connect } from 'react-redux'
-import { Navbar, NavItem } from 'react-materialize'
+import { Navbar, NavItem, Icon } from 'react-materialize'
 import {
   fetchGalleries,
   fetchGalleriesLists,
   setCurrentGalleries,
-  fetchGalleriesNotInLists
+  fetchGalleriesNotInLists,
+  setGalleriesPage
 } from 'nav-redux/actions'
 import {
   galleriesSelector,
   galleriesListsSelector,
   galleriesNotInListsSelector,
   currentGalleriesSelector,
-  currentGallerieSelector
+  currentGallerySelector,
+  galleriesPageSelector
 } from 'nav-redux/selectors'
 import { withRouter } from 'react-router'
 import utils from 'utils'
@@ -25,14 +27,12 @@ import './styles/Nav.css'
 
 import logo from './styles/img/logo.png'
 
+const galleriesNumToDisplay = 3
+
 const items = [
   {
     nameItem: 'Accueil',
     route: '/'
-  },
-  {
-    nameItem: 'UploadImage',
-    route: '/uploadimage'
   },
   {
     nameItem: 'Gallery',
@@ -50,8 +50,9 @@ const Nav = connect(
       galleries: galleriesSelector(state),
       galleriesLists: galleriesListsSelector(state),
       currentGalleries: currentGalleriesSelector(state),
-      currentGallerie: currentGallerieSelector(state),
-      galleriesNotInLists: galleriesNotInListsSelector(state)
+      currentGallery: currentGallerySelector(state),
+      galleriesNotInLists: galleriesNotInListsSelector(state),
+      galleriesPage: galleriesPageSelector(state)
     }
   },
   dispatch => ({
@@ -59,6 +60,10 @@ const Nav = connect(
       fetchGalleriesLists: () => fetchGalleriesLists()(dispatch),
       setCurrentGalleries: galleries => dispatch(setCurrentGalleries(galleries)),
       fetchGalleriesNotInLists: () => fetchGalleriesNotInLists()(dispatch),
+      setGalleriesPage: (e, page) => {
+        e.preventDefault();
+        dispatch(setGalleriesPage(page))
+      }
   })
 )(class extends React.Component {
 
@@ -81,10 +86,11 @@ const Nav = connect(
     const galleryId = utils.getIdFromPath(pathname)
     if(this.props.galleriesLists && this.props.galleries && this.props.location.pathname.includes("/gallery/") &&
       (!this.props.currentGalleries || prevGalleryId !== galleryId)){
-      var currentGallery = this.props.galleries.toJS().filter( gallery => gallery.id === galleryId)[0]
-      var currentList = this.props.galleriesLists.toJS().filter( list => list.id === currentGallery.parent_id)[0]
-      if( currentList !== undefined) this.props.setCurrentGalleries(this.props.galleries.toJS().filter( gallery => gallery.parent_id === currentList.id))
-    } else if (!this.props.location.pathname.includes("/gallery/")) {
+      var currentGallery = this.props.galleries.filter( gallery => gallery.id === galleryId)[0]
+      var currentList = this.props.galleriesLists.filter( list => list.id === currentGallery.parent_id)[0]
+      if( currentList !== undefined) this.props.setCurrentGalleries(this.props.galleries.filter( gallery => gallery.parent_id === currentList.id))
+    } else if (!this.props.location.pathname.includes("/gallery/")
+      && this.props.currentGalleries !== undefined) {
       this.props.setCurrentGalleries(undefined)
     }
   }
@@ -112,11 +118,36 @@ const Nav = connect(
       )
     }
     return(
-      <NavItem className='tab col'>
+      <NavItem className='tab col' s={6}>
         <Link to={`/gallery/${gallery.id}`}>{upperFirst(gallery.name)}</Link>
       </NavItem>
     )
+  }
 
+  getGalleriesToDisplay(){
+    return remove(map(this.props.currentGalleries, (gallerie, index) => {
+
+      if(index >= (this.props.galleriesPage - 1) * galleriesNumToDisplay
+        && index < this.props.galleriesPage * galleriesNumToDisplay) return gallerie
+    }), gallerie => gallerie !==undefined)
+  }
+
+  displayRightArrow () {
+    if(this.props.currentGalleries !== undefined) {
+      const lastDisplayed = this.getGalleriesToDisplay()[this.getGalleriesToDisplay().length - 1]
+      const lastDiplayedIndex = findIndex(this.props.currentGalleries, gallerie => lastDisplayed.id === gallerie.id)
+      return this.props.currentGalleries[lastDiplayedIndex + 1 ] !== undefined
+    }
+    return false
+  }
+
+  displayLeftArrow () {
+    if(this.props.currentGalleries !== undefined) {
+      const firstDisplayed = this.getGalleriesToDisplay()[0]
+      const firstDiplayedIndex = findIndex(this.props.currentGalleries, gallerie => firstDisplayed.id === gallerie.id)
+      return this.props.currentGalleries[firstDiplayedIndex - 1 ] !== undefined
+    }
+    return false
   }
 
   render () {
@@ -141,13 +172,14 @@ const Nav = connect(
           { currentGalleries
             && galleriesNotInLists
             && !map(
-              this.props.galleriesNotInLists.toJS(),
-              gallery => gallery.id).includes(utils.getIdFromPath(this.props.location.pathname))
-            && <Navbar className='galerie-nav tabs z-depth-1'>
-              {map(this.props.currentGalleries.toJS(), gallery => this.getNavItemsGalleries(gallery)
-
-              )}
-            </Navbar>
+              galleriesNotInLists,
+              gallery => gallery.id).includes(utils.getIdFromPath(location.pathname))
+            && <div className='galerie-nav tabs z-depth-1' brand='<Icon>navigate_before</Icon>' left>
+              {this.displayLeftArrow() && <NavItem className='tab col arrow'><a onClick={(e) => setGalleriesPage(e, galleriesPage - 1)} ><Icon>navigate_before</Icon></a></NavItem>}
+              {this.getGalleriesToDisplay().map(gallery => this.getNavItemsGalleries(gallery))
+              }
+              {this.displayRightArrow() && <NavItem className='tab col arrow'><a onClick={(e) => setGalleriesPage(e, galleriesPage + 1)} ><Icon>navigate_next</Icon></a></NavItem>}
+            </div>
           }
 
         </div>
