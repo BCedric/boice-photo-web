d2c71511ae60f14ed83b0f4f05e063a43a338b42
BCedric <skalimero@gmx.fr>
2019-11-08 18:53:17 +0100

    add galleries admin view

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -3,8 +3,10 @@ import Helmet from 'react-helmet'
 import {
   Button,
   Row,
-  Input
+  Input,
 } from 'react-materialize'
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import React from 'react'
 
 import {
@@ -15,6 +17,20 @@ import {
   updateDB,
   saveAdressServer
 } from 'admin-redux/actions.js'
+import AdminGalleries from './admin-components/AdminGalleries'
+import './styles/Admin.css'
+
+function TabPanel(props) {
+  const { children, value, index } = props;
+
+  return (
+    <div
+      hidden={value !== index}
+    >
+      {children}
+    </div>
+  );
+}
 
 const Admin = connect(
   state => {
@@ -31,31 +47,58 @@ const Admin = connect(
   }
 )(
   class extends React.Component {
-    constructor () {
+    constructor() {
       super()
-      this.state = { adressServer: '' }
+      this.state = { adressServer: '', value: 0 }
+    }
+
+    handleChange = (event, value) => {
+      this.setState({ value })
     }
 
 
-    render () {
+    render() {
+      const { value } = this.state
       return (
         <div>
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Boïce Photo | Admin </title>
-        </Helmet>
-          <form onSubmit={e => {
-            e.preventDefault()
-          }}>
-            <Row>
-            	<Input s={6} label="Adress Server" validate onChange={e =>{
-                this.props.saveAdressServer(e.target.value)
-              } } value={this.props.adressServer} />
-              <Button>Save</Button>
-            </Row>
-          </form>
-          <Button onClick={() => this.props.updateDB(this.props.adressServer)}>Update DB</Button>
-          {this.props.message && <p>{this.props.message}</p>}
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Boïce Photo | Admin </title>
+          </Helmet>
+
+          <Tabs
+            value={value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+          >
+            {/* <Tab title="Photos">
+              Photos
+            </Tab> */}
+            <Tab label="Galeries">
+            </Tab>
+            <Tab label="tests">
+
+            </Tab>
+          </Tabs>
+
+          <TabPanel value={value} index={0}>
+            <AdminGalleries></AdminGalleries>
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <form onSubmit={e => {
+              e.preventDefault()
+            }}>
+              <Row>
+                <Input s={6} label="Adress Server" validate onChange={e => {
+                  this.props.saveAdressServer(e.target.value)
+                }} value={this.props.adressServer} />
+                <Button>Save</Button>
+              </Row>
+            </form>
+            <Button onClick={() => this.props.updateDB(this.props.adressServer)}>Update DB</Button>
+            {this.props.message && <p>{this.props.message}</p>}
+          </TabPanel>
 
         </div>
       )
